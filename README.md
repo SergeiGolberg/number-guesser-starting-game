e45157a1537e07d109f9dad141cc7fc7bafee00d
Sergei_Golberg <golberg.sergei@gmail.com>
2023-09-19 20:08:35 +0400

    add game function and README

---
+++ README.md
@@ -0,0 +1,15 @@
+# NUMBER GUESSER game
+
+## It's my studying project from ***codecademy.com***
+
+### Game function:
+*In the user window you must select a number from 0 to 9; <br>
+*Press the button 'Make a Guess'; <br>
+*If the difference between your number and the secret target number is less than the difference between the random computer number and the secret number - you are winner of the round, You get a point; <br><br>
+
+
+### Additional Information:
++Updating the round counter; <br>
++The secret number is revealed after each round; <br>
++Updating points; <br>
++If you enter a number less than 0 or greater than 9, you will see an error message. <br>
--- script.js
+++ script.js
@@ -2,5 +2,43 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+// this function is for random computer number
+const generateTarget = () => {
+    return Math.floor(Math.random() * 10);
+}
+
+// function getting a winner
+const compareGuesses = (humanGuess, computerGuess, targetGuess) => {
+    
+        if (humanGuess < 0 || humanGuess > 9) {
+            alert('You must select a number from 0 to 9')
+        }
+    
+
+    const userDifference = Math.abs(humanGuess - targetGuess);
+    const computerDifference = Math.abs(computerGuess - targetGuess);
+
+    if (userDifference <= computerDifference) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// function which update scores
+const updateScore = winner => {
+    if (winner === 'human') {
+        humanScore++;
+    } else if (winner = 'computer') {
+        computerScore++;
+    }
+}
+
+// function updating a round
+const advanceRound = () => {
+    currentRoundNumber++;
+}
+
+
+
 
